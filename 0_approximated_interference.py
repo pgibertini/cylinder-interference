8519687d30bee718ca71e138fe4231886180dfa7
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2022-11-29 16:40:59 +0100

    update 0

--- 0_approximated_interference.py
+++ 0_approximated_interference.py
@@ -13,37 +13,42 @@ from volmdlr.primitives3d import Cylinder
 RADIUS = 0.05
 LENGTH = 0.1
 
-cyl1 = Cylinder(
-    position=volmdlr.O3D,
-    axis=volmdlr.X3D,
-    radius=RADIUS,
-    length=LENGTH,
-    color=(1, 0, 0),
-)
-cyl2 = Cylinder(
-    position=volmdlr.Point3D(0.01, 0.05, 0.005),
-    axis=volmdlr.Vector3D(1, 1, 1),
-    radius=RADIUS,
-    length=LENGTH,
-    color=(0, 0, 1),
-)
 
-VolumeModel([cyl1, cyl2]).babylonjs()
+def main():
+    cyl1 = Cylinder(
+        position=volmdlr.O3D,
+        axis=volmdlr.X3D,
+        radius=RADIUS,
+        length=LENGTH,
+        color=(1, 0, 0),
+    )
+    cyl2 = Cylinder(
+        position=volmdlr.Point3D(0.01, 0.05, 0.005),
+        axis=volmdlr.Vector3D(1, 1, 1),
+        radius=RADIUS,
+        length=LENGTH,
+        color=(0, 0, 1),
+    )
 
-ax = volmdlr.O3D.plot()
+    VolumeModel([cyl1, cyl2]).babylonjs()
 
-points_cyl1 = [p for p in cyl1.lhs_points_inside(1000) if not cyl2.point_belongs(p)]
-points_cyl2 = [p for p in cyl2.lhs_points_inside(1000) if not cyl1.point_belongs(p)]
-points_inter = [p for p in cyl1.lhs_points_inside(1000) if cyl2.point_belongs(p)]
+    ax = volmdlr.O3D.plot()
 
-for p in points_cyl1:
-    p.plot(ax=ax, color="red")
+    points_cyl1 = [p for p in cyl1.lhs_points_inside(1000) if not cyl2.point_belongs(p)]
+    points_cyl2 = [p for p in cyl2.lhs_points_inside(1000) if not cyl1.point_belongs(p)]
+    points_inter = [p for p in cyl1.lhs_points_inside(1000) if cyl2.point_belongs(p)]
 
-for p in points_cyl2:
-    p.plot(ax=ax, color="blue")
+    for p in points_cyl1:
+        p.plot(ax=ax, color="red")
 
-for p in points_inter:
-    p.plot(ax=ax)
+    for p in points_cyl2:
+        p.plot(ax=ax, color="blue")
+
+    for p in points_inter:
+        p.plot(ax=ax)
+
+    print(f"% of intersection : {(len(points_inter) / len(points_cyl1)) * 100}%")
 
 
-print(f"% of intersection : {(len(points_inter) / len(points_cyl1)) * 100}%")
+if __name__ == "__main__":
+    main()
